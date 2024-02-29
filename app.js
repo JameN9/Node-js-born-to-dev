c9cf865726df4282d53726defefe0f496bf911f7
JameN9 <jamedas3654@gmail.com>
2024-02-29 17:19:27 +0700

    add template website
    เพิ่ม template

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
+app.use(express.static(path.join(__filename,"/public/img/")));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("dasdsadas");
 });
 
 app.listen(port, () => {
