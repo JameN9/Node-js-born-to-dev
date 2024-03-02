851fc3fbad537b7b3d6b0c39ae882235ebffb40f
JameN9 <jamedas3654@gmail.com>
2024-03-02 14:24:03 +0700

    data json  ,route
    ทำความรู้จักกับ route และ json

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
 const path = require('path');
+const products = require("./data/products.json");
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT;
@@ -13,9 +15,17 @@ app.use(express.static(path.join(__dirname,"/public/")));
 app.set('views','./src/views');
 app.set('view engine', 'ejs');
 
+app.use("/products" ,productRouter)
+
+productRouter.route("/").get((req,res) =>{
+  res.render('products',products,);
+
+});
+
 app.get("/", (req, res) => {
   res.render('index',{username:'jame',custommer:['kitt','jame','joke']});
 });
+
 
 app.listen(PORT, () => {
   debug("It's port" + chalk.green(" : " + PORT));
